1bf2295273642deb7ed0282d4dfc73e85ef0d5c1
umiyosh <yumino99@gmail.com>
2024-09-25 23:46:11 +0900

    chore: update prompt to minimal form

--- config.py
+++ config.py
@@ -1,30 +1,26 @@
 SEGMENTS = [
-# Set the terminal window title to user@host:dir
-    'set_term_title',
-
-    'gke',
-    'gcp',
-    'newline',
-
-    'virtual_env',
-    'gvm',
-    'git',
-    'hg',
-    'svn',
-    'fossil',
-    'newline',
-
-    'username',
-    'hostname',
-    'ssh',
-    'cwd',
-    'read_only',
-
-    'newline',
-    'time',
-    'exit_code',
-    'root',
+    # Set the terminal window title to user@host:dir
+    "set_term_title",
+    "gke3",
+    "gcp",
+    "newline",
+    "virtual_env",
+    "gvm",
+    "git",
+    # #  'hg',
+    # #  'svn',
+    # #  'fossil',
+    "newline",
+    #  'username',
+    #  'hostname',
+    "ssh",
+    "cwd",
+    "read_only",
+    "newline",
+    "time",
+    "exit_code",
+    "root",
 ]
 
 # Change the colors used to draw individual segments in your prompt
-THEME = 'default'
+THEME = "default"
